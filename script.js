d95ae9f18d0977e0e34d328d140dbf7466ccd0f8
alexmatros <47996580+alexmatros@users.noreply.github.com>
2023-09-10 16:25:51 -0400

    Fixed event listener bugs

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 import { boardState } from "/board.js";
 
-const resetButton = document.getElementById("reset");
-resetButton.addEventListener("click", boardState.resetBoard());
-
 function main() {
   boardState.generateBoard();
 }
